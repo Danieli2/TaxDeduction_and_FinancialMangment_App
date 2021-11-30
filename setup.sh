b4ba8551006420c019978021d4d2acf5c473967e
Anna <nssensalo@gmail.com>
2021-11-29 20:44:36 -0800

    changes to setup file

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 mkdir -p ~/.streamlit/
 echo "\
+[theme]
+base='light'
+primaryColor='#77c19f'
+backgroundColor='#f5efef'
+secondaryBackgroundColor='#d6b361'
+font='monospace'
 [server]\n\
 headless = true\n\
 port = $PORT\n\
